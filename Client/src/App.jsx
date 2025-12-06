f2d3d59bce1c0fc50328ab6d6e0bee2d22dd6f7c
Nitish Kumar <kumarnitishbxr07@gmail.com>
2025-12-06 22:46:11 +0530

    Project initiate

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className=' bg-amber-200 '>
+        <h1 className='text-center'>Project Begin</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
